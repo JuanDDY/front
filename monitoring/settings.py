b7de11492b60615362fefa34c460eff52399983b
Juan Camilo Reyes <j.reyesc@uniandes.edu.co>
2022-11-18 20:56:16 -0500

    Arreglo Django

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -141,17 +141,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = "/login/auth0"
-LOGIN_REDIRECT_URL = "/" 
-LOGOUT_REDIRECT_URL = "https://isis2503-ivan-alfonso.auth0.com/v2/logout?returnTo=http%3A%2F%2Fip_publica_instancia:8080" 
-SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
-SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-ivan-alfonso.auth0.com' 
-SOCIAL_AUTH_AUTH0_KEY = 'W8g5KLG4s2ogftLqVDrGwd3xD7JafO0S' 
-SOCIAL_AUTH_AUTH0_SECRET = '7MVp47TDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
-SOCIAL_AUTH_AUTH0_SCOPE = [ 
-                            'openid', 
-                            'profile',
-                            'email',
-                            'role', ] 
-AUTHENTICATION_BACKENDS = { 'monitoring.auth0backend.Auth0',
-                            'django.contrib.auth.backends.ModelBackend', }
+
